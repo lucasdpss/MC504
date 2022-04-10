c2265e548ddb4b3f841ad36695a79ab336daf067
Antonio Gabriel da Silva Fernandes <antgabrielsf@gmail.com>
2022-04-10 13:27:23 -0300

    Adicionados comentários

--- projeto01/gameoflife.c
+++ projeto01/gameoflife.c
@@ -6,6 +6,7 @@
 #define QUAD_WDT 5
 #define QUAD_HGT 5
 
+// Struct usada para enviar os argumentos da função f_thread
 typedef struct {
     char **matriz;
     char **prox_matriz;
@@ -20,8 +21,8 @@ int get_vizinhos_vivos(char **matriz, int M, int N, int i, int j){
 
     for(int x = i - 1 ; x <= i+1 ; x++){
         for(int y = j - 1 ; y <= j+1 ; y++){
-            if(x < 0 || x >= M || y < 0 || y >= N) continue; // Celula fora da matriz
-            if(x == i && y == j) continue; // Propria celula
+            if(x < 0 || x >= M || y < 0 || y >= N) continue; // Célula fora da matriz
+            if(x == i && y == j) continue; // Própria célula
             count += (matriz[x][y] == '#' ? 1 : 0);
         }
     }
@@ -29,11 +30,12 @@ int get_vizinhos_vivos(char **matriz, int M, int N, int i, int j){
     return count;
 }
 
+// Processa o quadrante da matriz associado à ID (i, j) da thread
 void* f_thread(void *void_args) {
     f_thread_args *args = (f_thread_args*) void_args;
     for(int x = args->i * QUAD_HGT ; x < (args->i + 1) * QUAD_HGT ; x++){
         for(int y = args->j * QUAD_WDT ; y < (args->j + 1) * QUAD_WDT ; y++){
-            if(x < 0 || x >= args->M || y < 0 || y >= args->N) continue; // Celula fora da matriz
+            if(x < 0 || x >= args->M || y < 0 || y >= args->N) continue; // Célula fora da matriz
             int vizinhos = get_vizinhos_vivos(args->matriz, args->M, args->N, x, y);
             if(args->matriz[x][y] == '#'){
                 if(vizinhos == 2 || vizinhos == 3) args->prox_matriz[x][y] = '#';
@@ -50,25 +52,27 @@ void* f_thread(void *void_args) {
     return NULL; 
 }
 
+// Aloca memória para uma matriz M x N
 char** malloc_matriz(int M, int N){
-  char **matriz;
-  int i;
- 
-  matriz = (char**) malloc(sizeof(char*) * M);
-  if(matriz == NULL){
-    printf("Memoria insuficiente.\n");
-    exit(1);
-  }
-  for(i = 0; i < M; i++){
-    matriz[i] = (char*) malloc(sizeof(char) * N);
-    if(matriz[i] == NULL){
-      printf("Memoria insuficiente.\n");
-      exit(1);
+    char **matriz;
+    int i;
+
+    matriz = (char**) malloc(sizeof(char*) * M);
+    if(matriz == NULL){
+        printf("Memoria insuficiente.\n");
+        exit(1);
     }
-  }
-  return matriz;
+    for(i = 0; i < M; i++){
+        matriz[i] = (char*) malloc(sizeof(char) * N);
+        if(matriz[i] == NULL){
+            printf("Memoria insuficiente.\n");
+            exit(1);
+        }
+    }
+    return matriz;
 } 
 
+// Libera a memória alocada para uma matriz com M linhas
 void free_matriz(char **matriz, int M){
     for(int i = 0 ; i < M ; i++){
         free(matriz[i]);
@@ -76,6 +80,7 @@ void free_matriz(char **matriz, int M){
     free(matriz);
 }
 
+// Imprime uma matriz M x N
 void print_matriz(char **matriz, int M, int N){    
     for(int i = 0 ; i < M ; i++){
         for(int j = 0 ; j < N ; j++){
@@ -85,7 +90,6 @@ void print_matriz(char **matriz, int M, int N){
     }
     printf("\n");
 }
-
 
 int main() {
     int M, N;
@@ -98,6 +102,7 @@ int main() {
         }
     }
 
+    // Cálculo da quantidade de quadrantes em cada dimensão:
     const int thr_num_hgt = M / QUAD_HGT + ((M % QUAD_HGT == 0) ? 0 : 1); // teto(M / QUAD_HGT)
     const int thr_num_wdt = N / QUAD_WDT + ((N % QUAD_WDT == 0) ? 0 : 1); // teto(N / QUAD_WDT)
     pthread_t thr[thr_num_hgt][thr_num_wdt];
@@ -108,6 +113,7 @@ int main() {
         for(int i = 0 ; i < thr_num_hgt ; i++){
             for(int j = 0 ; j < thr_num_wdt ; j++){
                 f_thread_args* args = malloc(sizeof(f_thread_args));
+                if(args == NULL) return 1;
                 args->matriz = matriz;
                 args->prox_matriz = prox_matriz;
                 args->M = M;
